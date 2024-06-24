b8c1ac899bf16d901d29a29db032f0ce184cb30a
Heitor <4553425989@estudante.sed.sc.gov.br>
2024-06-24 19:18:39 -0300

    Primeiro commit com Java escript

---
+++ e.js
@@ -0,0 +1,67 @@
+//1º
+let nome='Egntur'
+console.log(nome)
+
+//2º
+let a=5,b=10
+console.log('soma de a+b=',a+b)
+
+//3º
+let primeiroNome='Maria',ultimoNome='Aparecida'
+console.log(primeiroNome,ultimoNome)
+
+//4º
+let pessoa={nome:'Egntur',idade:14,telefone:30948230}
+console.log(pessoa)
+
+//5º
+let vetor=['maça','banana','laranja']
+vetor.push('uva')
+console.log(vetor)
+
+//6º
+vetor=[1,2,3,4,5]
+console.log(vetor[0]);
+console.log(vetor[4]);
+
+//7º
+cores=['verde','vermelho','azul']
+console.log(cores)
+
+//8º
+let idade=17
+
+if(idade>=18){
+    console.log('maior de idade')
+} else {
+    console.log('Menor de idade')
+}
+//9º
+let nota=4
+if(nota>=7){
+    console.log('Passou!!')
+}
+if(nota>=5 & nota<=6){
+    console.log('Rec')
+} else if (nota < 5){
+    console.log('Reprovou')
+}
+//10ª
+let diaDaSemana=[1,2,3,4,5,6,7];
+switch (diaDaSemana) {
+    case 1:
+        console.log('segunda')
+        break
+        case 2:
+        console.log('terca')
+        break
+        case 3:
+        console.log('quarta')
+        break
+        case 4:
+        
+        
+        
+
+    
+}
